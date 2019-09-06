cc756ad02013b6c85b71bae83281fef4d3a51219
hanh01 <54634385+hanh01@users.noreply.github.com>
2019-09-06 09:58:47 +0700

    bai tap 4

---
+++ bai tap 3.2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	int n;
+	scanf("%d",&n);
+	int thu=2;
+	while(thu<n){
+	if(n<=8){
+		printf("thu=%d \n",thu);
+	}else{
+} 
+      thu++;
+      
+	}
+	return 0;
+}
