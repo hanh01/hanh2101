a42bb3eb5f691e9afedfd1362922e989d9bd57f3
hanh01 <54634385+hanh01@users.noreply.github.com>
2019-09-07 10:08:12 +0700

    bai tap 5

---
+++ bai tap 5.1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	printf("nhap n \n");
+	int n;
+	scanf("%d",&n);
+	int dem=1;
+   for(n!=0;n=n/10;dem++){
+   }
+	    printf("so cac chu so cua n la :%d\n",dem);
+		
+	    
+	
+	return 0;
+}
