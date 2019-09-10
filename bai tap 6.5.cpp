f4701a00f6e5ad914603e01d0989b793698ab78a
hanh01 <54634385+hanh01@users.noreply.github.com>
2019-09-10 13:34:28 +0700

    Create bai tap 6.5.cpp

---
+++ bai tap 6.5.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+	int ary[20];
+       	for(int i=2;i<20;i++){
+         scanf("%d",&ary[i]);
+        for(int count=0;count++;){
+        	if(ary[i]%i==0){
+        		count++;
+			}
+			if(ary[count>2]){
+				 printf("khong phai la so nguyen to");
+			}
+			 else{
+			 	printf("la so nguyen to" );
+			 }
+		}
+    
+}
+	return 0;
+	
+}
