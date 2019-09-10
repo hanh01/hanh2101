24a6b8f019d30e8420d6d2ec7830690911c7ecef
hanh01 <54634385+hanh01@users.noreply.github.com>
2019-09-10 11:26:10 +0700

    Update bai tap 6.3.cpp

--- bai tap 6.3.cpp
+++ bai tap 6.3.cpp
@@ -1,15 +1,23 @@
 #include <stdio.h>
 int main(){
-    	int ary[6];
+    	int a[6];
 	for(int i=0;i<6;i++){
-	  	scanf("%d",&ary[i]);
+	  	scanf("%d",&a[i]);
 }
-        printf("          *\n");
-        printf("          **\n");
-        printf("          ***\n");
-        printf("          ****\n");
-        printf("          *****\n");
-        printf("          ******\n");
+    for(int i=0;i<6;i++){
+     if(a[i]==0){
+        printf("          *\n");}
+      if(a[i]==1){  
+        printf("          **\n");}
+      if(a[i]==2){ 
+        printf("          ***\n");}
+      if(a[i]==3){
+        printf("          ****\n");}
+      if(a[i]==4){  
+        printf("          *****\n");}
+      if(a[i]==5){  
+        printf("          ******\n");}
+}
 
 	return 0;
 }
