d68a4c6f996a34b8f24965232b5de1985c16f46f
hanh01 <54634385+hanh01@users.noreply.github.com>
2020-04-22 20:27:17 +0700

    Add files via upload

---
+++ Json/MLJ_07/Main.java
@@ -0,0 +1,28 @@
+package Json.MLJ_07;
+
+import org.json.simple.*;
+import org.json.simple.parser.*;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        JSONParser parser = new JSONParser();
+        try{
+            Object obj = parser.parse(new FileReader("course.json"));
+            JSONObject jsonObject = (JSONObject)obj;
+            String name = (String)jsonObject.get("Name");
+            String course = (String)jsonObject.get("Course");
+            JSONArray subjects = (JSONArray)jsonObject.get("Subjects");
+            System.out.println("Name" + name);
+            System.out.println("Course" + course);
+            System.out.println("Subjects : " );
+            Iterator iterator = subjects.iterator();
+            while (iterator.hasNext()){
+                System.out.println(iterator.next());
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
