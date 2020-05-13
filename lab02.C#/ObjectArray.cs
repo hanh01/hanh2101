f6ce3a596fd87f00d20b50d5acb2b8cc9e19fd10
hanh01 <54634385+hanh01@users.noreply.github.com>
2020-05-13 20:56:16 +0700

    Lab02.C#

---
+++ lab02.C#/ObjectArray.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Employee
+{
+    class Employee
+    {
+        private int empID;
+        public Employee(int empID)
+        {
+            this.empID = empID;
+        }
+        public override string ToString()
+        {
+            return empID.ToString();
+        }
+    }
+    class ObjectArray
+    {
+        public void Run()
+        {
+            int[] intArray;
+            Employee[] empArray;
+            intArray = new int[5];
+            empArray = new Employee[3];
+            for (int i = 0; i< empArray.Length; i++)
+            {
+                empArray[i] = new Employee(i + 5);
+            }
+            Console.WriteLine("The int array...");
+            for(int i = 0; i < intArray.Length; i++)
+            {
+                Console.WriteLine(intArray[i].ToString());
+            }
+            Console.WriteLine("\nThe employee array...");
+            for(int i = 0;i < empArray.Length; i++)
+            {
+                Console.WriteLine(empArray[i].ToString());
+            }
+        }
+        static void Main(string[] args)
+        {
+            ObjectArray arr = new ObjectArray();
+            arr.Run();
+            Console.ReadLine();
+        }
+    }
+}
